e7256f524afc10550115f304f9fd302c0ebc8e37
Evelyne003 <evelynerv03@gmail.com>
2025-11-03 02:22:21 +0000

    feat: Implement text cleaning and OCR functions

---
+++ src/processing.py
@@ -0,0 +1,36 @@
+import re
+import pytesseract
+from PIL import Image
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+# Configuramos las stopwords 
+# Tanto en español e inglés
+stop_words = set(stopwords.words('english')).union(set(stopwords.words('spanish')))
+
+def clean_text(text): # Limpia el texto: minúsculas, quita stopwords y caracteres no alfabéticos.
+    
+    if not isinstance(text, str):
+        return ""
+
+    text = text.lower() # A minúsculas
+    text = re.sub(r'[^a-z\s]', '', text) # Solo letras y espacios
+    tokens = word_tokenize(text)
+
+    # Quitar stopwords
+    cleaned_tokens = [w for w in tokens if w not in stop_words]
+
+    return " ".join(cleaned_tokens)
+
+def extract_text_from_image(image_path):
+    try:
+        # Abrir la imagen usando Pillow 
+        img = Image.open(image_path)
+
+        # Usar pytesseract para "leer" la imagen
+        text = pytesseract.image_to_string(img)
+
+        return text
+    except Exception as e:
+        print(f"Error de OCR en {image_path}: {e}")
+        return "" # Devuelve vacío si falla
